7ba5d1f6768eb635fe9f70463d4bd787423d4cf7
tepcii <pepci.oishii@gmail.com>
2022-01-12 15:08:49 +0900

    show toast

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -1,11 +1,9 @@
 import Toast from './Toast'
 import { defineComponent } from "vue";
+import { mount } from 'mount-vue-component'
 
-import config, { VueInstance } from '../../utils/config'
+import config from '../../utils/config'
 import { merge } from '../../utils/helpers'
-import { use, registerComponentProgrammatic } from '../../utils/plugins'
-
-let localVueInstance
 
 const ToastProgrammatic = {
     open(params) {
@@ -23,27 +21,12 @@ const ToastProgrammatic = {
             parent = params.parent
             delete params.parent
         }
-        let slot
-        if (Array.isArray(params.message)) {
-            slot = params.message
-            delete params.message
-        }
         const propsData = merge(defaultParam, params)
-        const toastComponent = defineComponent(Toast)
-        if (slot) {
-            toastComponent.slot = slot
-        }
-        console.log(toastComponent)
-        // const component = toastComponent({
-        //     parent,
-        //     el: document.createElement('div'),
-        //     propsData
-        // })
-        // if (slot) {
-        //     component.$slots.default = slot
-        //     component.$forceUpdate()
-        // }
-        return toastComponent
+        const ToastComponent = defineComponent({
+            extends: Toast,
+        })
+        mount(ToastComponent, { props: propsData })
+        return ToastComponent
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -12,22 +12,22 @@ const VuetifyFloats = {
 
         // app.provide('FToast', ToastProgrammatic)
 
-        function inject(key) {
-            var _vm$parent$provides, _vm$parent, _vm$vnode$appContext;
+        // function inject(key) {
+        //     var _vm$parent$provides, _vm$parent, _vm$vnode$appContext;
 
-            const vm = this.$;
-            const provides = (_vm$parent$provides = (_vm$parent = vm.parent) == null ? void 0 : _vm$parent.provides) != null ? _vm$parent$provides : (_vm$vnode$appContext = vm.vnode.appContext) == null ? void 0 : _vm$vnode$appContext.provides;
+        //     const vm = this.$;
+        //     const provides = (_vm$parent$provides = (_vm$parent = vm.parent) == null ? void 0 : _vm$parent.provides) != null ? _vm$parent$provides : (_vm$vnode$appContext = vm.vnode.appContext) == null ? void 0 : _vm$vnode$appContext.provides;
 
-            if (provides && key in provides) {
-                return provides[key];
-            }
-        }
+        //     if (provides && key in provides) {
+        //         return provides[key];
+        //     }
+        // }
 
         app.mixin({
             computed: {
                 $floats() {
                     return reactive({
-                        toast: inject.call(this, 'FToast'),
+                        // toast: inject.call(this, 'FToast'),
                         // toast: ToastProgrammatic,
                         msg: 'hello'
                     })
